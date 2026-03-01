b3e5df575531ddda696ce5ef6b858f31f64e3375
olayemi omolisa <olayemiomolisa@yahoo.com>
2026-03-01 10:49:11 +0100

    server port change

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,6 @@ import userRouter from "./Routes/userRouter.js";
 
 // app config
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 
 // middleware
@@ -29,6 +28,8 @@ app.get("/", (req,res)=> {
     res.send("api initiating")
 })
 
-app.listen(PORT,()=>{
-    console.log(`server running on http://localhost:${PORT}`)
-})
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
